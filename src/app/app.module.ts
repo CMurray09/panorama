456f1c72f68aefff6da91fd41b8e253a4e23c7e9
Cameron M <cameronmurray@hotmail.ca>
2022-10-22 21:58:53 +0100

    Login, register components, firebase, and more setups

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageViewerComponent } from './image-viewer/image-viewer.component';
+import { AngularFireModule } from "@angular/fire/compat";
+import { environment } from "../environments/environment";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { AuthModalComponent } from './user/auth-modal/auth-modal/auth-modal.component';
+import { NavComponent } from './nav/nav/nav.component';
+import { NotFoundComponent } from './not-found/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageViewerComponent,
+    LoginComponent,
+    RegisterComponent,
+    AuthModalComponent,
+    NavComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
