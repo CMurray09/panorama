ed2d6970f99e2b614ae2eee7c4b8cd310fb5c24b
Cameron M <cameronmurray@hotmail.ca>
2022-11-21 23:10:17 +0000

    Image upload fixes

--- src/app/media/upload/upload.component.ts
+++ src/app/media/upload/upload.component.ts
@@ -7,11 +7,12 @@ import {AngularFireAuth} from "@angular/fire/compat/auth";
 import firebase from 'firebase/compat/app';
 import {ClipService} from "src/app/services/clip.service";
 import {Router} from '@angular/router';
-import {combineLatest} from 'rxjs';
+import {combineLatest, finalize, forkJoin, switchMap} from 'rxjs';
 import Image from "../../models/clip.model";
 import {DocumentReference} from "@angular/fire/compat/firestore";
 import {FfmpegService} from "../../services/ffmpeg.service";
 import {NgxFileDropEntry} from "ngx-file-drop";
+import {FFmpeg} from "@ffmpeg/ffmpeg";
 
 @Component({
   selector: 'app-upload',
@@ -20,7 +21,7 @@ import {NgxFileDropEntry} from "ngx-file-drop";
 })
 export class UploadComponent implements OnDestroy {
   isDragover: boolean = false;
-  file: File | null = null;
+  files: File[] = [];
   isMediaDropped: boolean = false;
   showAlert: boolean = false;
   alertColour: string = 'red';
@@ -32,13 +33,18 @@ export class UploadComponent implements OnDestroy {
   task?: AngularFireUploadTask;
   image: string = '';
   imageTask: AngularFireUploadTask | undefined;
+  screenshotTask?: AngularFireUploadTask;
+
+  screenshots: Array<string> = [];
+
+  images: Array<string> = [];
+
+  private ffmpeg: FFmpeg | undefined;
 
   currentFileCount: number = 0;
   totalFileCount: number = 0;
   formData = new FormData()
 
-
-  public files: NgxFileDropEntry[] = [];
 
   title = new FormControl('', {
     validators: [
@@ -67,102 +73,129 @@ export class UploadComponent implements OnDestroy {
     this.task?.cancel();
   }
 
+  checkFile(droppedFile: any) {
+    let invalid = true;
+    if (droppedFile.fileEntry.isFile) {
+      this.showAlert = false;
+      const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+      fileEntry.file((file: File) => {
+        if (!file.type.includes('image/')) {
+          this.showAlert = true;
+          this.alertMsg = 'The dropped file is not an image'
+          return;
+        }
+        this.formData.append('logo', file, droppedFile.relativePath);
+        this.totalFileCount++;
+
+        // this.title.setValue(file.name.replace(/\.[^/.]+$/, ''));
+        this.isMediaDropped = true;
+
+        // Here you can access the real file
+        console.log(droppedFile.relativePath, file);
+
+        invalid = false;
+
+      });
+    } else {
+      this.showAlert = true;
+      this.alertMsg = 'Something went wrong. Try again.'
+      this.files = [];
+      return;
+    }
+    return invalid;
+  }
+
 
   public dropped(files: NgxFileDropEntry[]) {
-    this.files = files;
-    this.totalFileCount = 0;
+    this.isMediaDropped = true;
     for (const droppedFile of files) {
-      // Is it a single file?
-      if (droppedFile.fileEntry.isFile) {
-        this.showAlert = false;
-        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
-        fileEntry.file((file: File) => {
-          if (!file.type.includes('image/')) {
-            this.showAlert = true;
-            this.alertMsg = 'The dropped file is not an image'
-            return;
-          }
-          this.formData.append('logo', file, droppedFile.relativePath);
-          this.totalFileCount++;
 
-          // this.title.setValue(file.name.replace(/\.[^/.]+$/, ''));
-          this.isMediaDropped = true;
+      // Is it a file?
+      if (droppedFile.fileEntry.isFile) {
+        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+        fileEntry.file(async (file: File) => {
+          this.files.push(file);
+          this.screenshots.push(await this.ffmpegService.getScreenshot(file));
 
           // Here you can access the real file
           console.log(droppedFile.relativePath, file);
 
-
-        });
+        })
       } else {
-        this.showAlert = true;
-        this.alertMsg = 'Something went wrong. Try again.'
-        this.files = [];
-        return;
+        // It was a directory (empty directories are added, otherwise only files)
+        const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
+        console.log(droppedFile.relativePath, fileEntry);
       }
     }
-    console.log(this.files.length)
   }
 
   async uploadFile(): Promise<void> {
+    console.log(this.screenshots)
     this.mediaForm.disable();
     this.showAlert = true;
     this.alertColour = 'blue';
     this.alertMsg = 'Please wait! Your clip is being uploaded.';
     this.inSubmission = true;
     this.showPercentage = true;
+    const title: string = this.title.value;
+    const clipFileName: string = uuidv4();
 
-    for (const file of this.files) {
-      const title: string = this.title.value;
-      const imageFileName: string = uuidv4();
-      const image: string = await this.ffmpegService.createBlob(file);
-      const imageBlob: Blob = await this.ffmpegService.blobFromURL(image);
-      const imagePath: string = `images/${imageFileName}.png`;
+    const screenshotBlob: Blob = await this.ffmpegService.blobFromURL(
+      this.screenshots[0]
+    );
+    const screenshotPath: string = `screenshots/${clipFileName}.jpg`;
+    console.log(screenshotBlob);
 
-      this.imageTask = this.storage.upload(imagePath, imageBlob);
-      const imageRef: AngularFireStorageReference = this.storage.ref(imagePath);
+    this.screenshotTask = this.storage.upload(screenshotPath, screenshotBlob);
+    const screenshotRef: AngularFireStorageReference = this.storage.ref(screenshotPath);
 
-      combineLatest([
-        this.imageTask.percentageChanges()
-      ]).subscribe((progress) => {
-        const imageProgress: any = progress;
-        if (!imageProgress) {
-          return;
+    combineLatest([
+      this.screenshotTask.percentageChanges()
+    ]).subscribe((progress) => {
+      const [clipProgress, screenshotProgress]: Array<number | undefined> = progress;
+      if (!clipProgress || !screenshotProgress) {
+        return;
+      }
+      const total: number = clipProgress + screenshotProgress;
+      this.percentage = total as number / 200;
+    });
+
+    forkJoin([
+      this.screenshotTask.snapshotChanges()
+    ]).pipe(
+      switchMap(() => forkJoin([
+        screenshotRef.getDownloadURL()
+      ]))
+    ).subscribe({
+      next: async (urls) => {
+        const [clipURL, screenshotURL]: Array<string> = urls;
+        const clip: Image = {
+          uid: this.user?.uid as string,
+          displayName: this.user?.displayName as string,
+          title,
+          fileName: `${clipFileName}.jpg`,
+          url: clipURL,
+          timestamp: firebase.firestore.FieldValue.serverTimestamp()
         }
-        this.percentage = imageProgress as number / 100;
-      });
+        const clipDocRef: DocumentReference<Image> = await this.clipsService.createClip(clip);
+        this.alertColour = 'green';
+        this.alertMsg = 'Success! Your clip is now ready to share with the world.';
+        this.showPercentage = false;
 
-      imageRef.getDownloadURL().subscribe({
-        next: async (url) => {
-          const imageURL: string = url;
-          const image: Image = {
-            uid: this.user?.uid as string,
-            displayName: this.user?.displayName as string,
-            title,
-            fileName: `${imageFileName}.png`,
-            url: imageURL,
-            timestamp: firebase.firestore.FieldValue.serverTimestamp()
-          }
-          const clipDocRef: DocumentReference<Image> = await this.clipsService.createClip(image);
-          this.alertColour = 'green';
-          this.alertMsg = 'Success! Your clip is now ready to share with the world.';
-          this.showPercentage = false;
-
-          setTimeout(() => {
-            this.router.navigate([
-              'image', clipDocRef.id
-            ])
-          }, 1000)
-        },
-        error: (error) => {
-          this.mediaForm.enable();
-          this.alertColour = 'red';
-          this.alertMsg = 'Upload failed! Please try again later.';
-          this.showPercentage = false;
-          this.inSubmission = true;
-          console.error(error);
-        }
-      });
-
-    }
+        setTimeout(() => {
+          this.router.navigate([
+            'clip', clipDocRef.id
+          ])
+        }, 1000)
+      },
+      error: (error) => {
+        this.mediaForm.enable();
+        this.alertColour = 'red';
+        this.alertMsg = 'Upload failed! Please try again later.';
+        this.showPercentage = false;
+        this.inSubmission = true;
+        console.error(error);
+      }
+    });
   }
 }
