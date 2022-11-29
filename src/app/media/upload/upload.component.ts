507e1caf8520b92ae69daa86362dfa39bacb6e3f
Cameron M <cameronmurray@hotmail.ca>
2022-11-29 00:47:43 +0000

    File valid refactoring

--- src/app/media/upload/upload.component.ts
+++ src/app/media/upload/upload.component.ts
@@ -7,12 +7,11 @@ import {AngularFireAuth} from "@angular/fire/compat/auth";
 import firebase from 'firebase/compat/app';
 import {ClipService} from "src/app/services/clip.service";
 import {Router} from '@angular/router';
-import {combineLatest, finalize, forkJoin, switchMap} from 'rxjs';
+import {forkJoin, switchMap} from 'rxjs';
 import Image from "../../models/clip.model";
 import {DocumentReference} from "@angular/fire/compat/firestore";
 import {FfmpegService} from "../../services/ffmpeg.service";
 import {NgxFileDropEntry} from "ngx-file-drop";
-import {FFmpeg} from "@ffmpeg/ffmpeg";
 
 @Component({
   selector: 'app-upload',
@@ -36,17 +35,24 @@ export class UploadComponent implements OnDestroy {
   screenshotTask?: AngularFireUploadTask;
 
   screenshots: Array<string> = [];
+  imageType: string = 'image/png';
 
   images: Array<string> = [];
 
-  private ffmpeg: FFmpeg | undefined;
-
-  currentFileCount: number = 0;
   totalFileCount: number = 0;
   formData = new FormData()
 
 
   title = new FormControl('', {
+    validators: [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(50)
+    ],
+    nonNullable: true
+  });
+
+  author = new FormControl('', {
     validators: [
       Validators.required,
       Validators.minLength(3),
@@ -73,12 +79,11 @@ export class UploadComponent implements OnDestroy {
     this.task?.cancel();
   }
 
-  checkFile(droppedFile: any) {
+  checkFileValid(droppedFile: any) {
     let invalid = true;
     if (droppedFile.fileEntry.isFile) {
-      this.showAlert = false;
       const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
-      fileEntry.file((file: File) => {
+      fileEntry.file(async (file: File) => {
         if (!file.type.includes('image/')) {
           this.showAlert = true;
           this.alertMsg = 'The dropped file is not an image'
@@ -86,15 +91,13 @@ export class UploadComponent implements OnDestroy {
         }
         this.formData.append('logo', file, droppedFile.relativePath);
         this.totalFileCount++;
-
-        // this.title.setValue(file.name.replace(/\.[^/.]+$/, ''));
         this.isMediaDropped = true;
 
-        // Here you can access the real file
-        console.log(droppedFile.relativePath, file);
-
+        // File check passed. Add to file array
         invalid = false;
-
+        this.files.push(file);
+        this.imageType = file.type;
+        this.screenshots.push(await this.ffmpegService.getScreenshot(file));
       });
     } else {
       this.showAlert = true;
@@ -107,30 +110,23 @@ export class UploadComponent implements OnDestroy {
 
 
   public dropped(files: NgxFileDropEntry[]) {
-    this.isMediaDropped = true;
+    if (files.length > 1) {
+      this.showAlert = true;
+      this.alertMsg = 'You cannot upload more than one file at once.';
+      this.files = [];
+      return;
+    }
+
     for (const droppedFile of files) {
-
-      // Is it a file?
-      if (droppedFile.fileEntry.isFile) {
-        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
-        fileEntry.file(async (file: File) => {
-          this.files.push(file);
-          this.screenshots.push(await this.ffmpegService.getScreenshot(file));
-
-          // Here you can access the real file
-          console.log(droppedFile.relativePath, file);
-
-        })
-      } else {
-        // It was a directory (empty directories are added, otherwise only files)
-        const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
-        console.log(droppedFile.relativePath, fileEntry);
-      }
+       if (this.checkFileValid(droppedFile)) {
+         return;
+       }
+      this.showAlert = false;
+      this.isMediaDropped = true;
     }
   }
 
   async uploadFile(): Promise<void> {
-    console.log(this.screenshots)
     this.mediaForm.disable();
     this.showAlert = true;
     this.alertColour = 'blue';
@@ -138,26 +134,23 @@ export class UploadComponent implements OnDestroy {
     this.inSubmission = true;
     this.showPercentage = true;
     const title: string = this.title.value;
+    const author: string = this.author.value;
     const clipFileName: string = uuidv4();
 
     const screenshotBlob: Blob = await this.ffmpegService.blobFromURL(
       this.screenshots[0]
     );
-    const screenshotPath: string = `screenshots/${clipFileName}.jpg`;
-    console.log(screenshotBlob);
+    const screenshotPath: string = `screenshots/${clipFileName}.${this.imageType}`;
 
     this.screenshotTask = this.storage.upload(screenshotPath, screenshotBlob);
     const screenshotRef: AngularFireStorageReference = this.storage.ref(screenshotPath);
 
-    combineLatest([
-      this.screenshotTask.percentageChanges()
-    ]).subscribe((progress) => {
-      const [clipProgress, screenshotProgress]: Array<number | undefined> = progress;
-      if (!clipProgress || !screenshotProgress) {
+      this.screenshotTask.percentageChanges().subscribe((progress) => {
+      const screenshotProgress: number | undefined = progress;
+      if (!screenshotProgress) {
         return;
       }
-      const total: number = clipProgress + screenshotProgress;
-      this.percentage = total as number / 200;
+      this.percentage = screenshotProgress as number / 100;
     });
 
     forkJoin([
@@ -168,13 +161,13 @@ export class UploadComponent implements OnDestroy {
       ]))
     ).subscribe({
       next: async (urls) => {
-        const [clipURL, screenshotURL]: Array<string> = urls;
+        const screenshotURL: any = urls;
         const clip: Image = {
           uid: this.user?.uid as string,
-          displayName: this.user?.displayName as string,
+          displayName: author ? author : this.user?.displayName as string,
           title,
           fileName: `${clipFileName}.jpg`,
-          url: clipURL,
+          url: screenshotURL,
           timestamp: firebase.firestore.FieldValue.serverTimestamp()
         }
         const clipDocRef: DocumentReference<Image> = await this.clipsService.createClip(clip);
